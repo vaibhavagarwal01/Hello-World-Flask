596ec7fad96d81489e9527ff0bc4475de3397468
Vaibhav <vaibhavagarwal3928@gmail.com>
2023-07-04 17:39:20 +0530

    added port

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ def testget():
     response["message"] = "Hello World"
     return jsonify(response)
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 
